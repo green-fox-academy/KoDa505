78b0633f8297453c55450a6e684f697b46a8080d
Daniella Korossy <koda5055@gmail.com>
2015-12-03 17:37:03 +0100

    Thursday practice

---
+++ Week-4/Thursday/arg.py
@@ -0,0 +1,8 @@
+import sys
+
+if (len(sys.argv) == 1):
+    print('No arguments')
+elif (sys.argv [1] == 'add'):
+    print('Added todo elem:' + sys.argv[2])
+else:
+    print('nem is kacsa')       
