1973c7bffc69304dcdc097b1e1291775464c3b23
Daniella Korossy <koda5055@gmail.com>
2015-12-14 18:45:34 +0100

    create quit submenu

---
+++ Week-6/RPG/quit_submenu.py
@@ -0,0 +1,14 @@
+from menu import *
+import commands_quit_submenu
+
+Quit_SubMenu = Menu()
+
+
+Menu1 = MenuItem(1, "Save and Quit", "com").item
+Menu2 = MenuItem(2, "Quit without save", "Continue").item
+Menu3 = MenuItem(3, "Resume", "save function").item
+
+
+Quit_SubMenu.add(Menu1)
+Quit_SubMenu.add(Menu2)
+Quit_SubMenu.add(Menu3)
