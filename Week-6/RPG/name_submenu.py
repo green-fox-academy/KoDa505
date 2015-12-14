73b62da88a312b49bcd5265c78316742a9b03601
Daniella Korossy <koda5055@gmail.com>
2015-12-14 18:44:54 +0100

    adds name submenu

---
+++ Week-6/RPG/name_submenu.py
@@ -0,0 +1,21 @@
+from menu import *
+import commands_quit_submenu
+
+Name_SubMenu = Menu()
+
+
+Menu1 = MenuItem(1, "Reenter name", commands_quit_submenu.reenter_name).item
+Menu2 = MenuItem(2, "Continue", "Continue function").item
+Menu3 = MenuItem(3, "Save", "save function").item
+Menu4 = MenuItem(4, "Quit", commands_quit_submenu.quitting).item
+
+
+Name_SubMenu.add(Menu1)
+Name_SubMenu.add(Menu2)
+Name_SubMenu.add(Menu3)
+Name_SubMenu.add(Menu4)
+
+
+#Name_SubMenu.printing_menu()
+
+#Name_SubMenu.choose()
