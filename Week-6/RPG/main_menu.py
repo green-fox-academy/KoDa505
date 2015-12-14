79097261e2ed7316d2387b921aca12551db56ac8
Daniella Korossy <koda5055@gmail.com>
2015-12-14 18:43:49 +0100

    adds the first step, starts the game

---
+++ Week-6/RPG/main_menu.py
@@ -0,0 +1,19 @@
+from menu import *
+import commands
+
+MainMenu = Menu()
+
+
+Menu1 = MenuItem(1, "New game", commands.new_game).item
+Menu2 = MenuItem(2, "Load game", "cmd").item
+Menu3 = MenuItem(3, "Exit", commands.exiting).item
+
+
+MainMenu.add(Menu1)
+MainMenu.add(Menu2)
+MainMenu.add(Menu3)
+
+
+MainMenu.printing_menu()
+
+MainMenu.choose()
---
+++ Week-6/RPG/main_menu_test.py
@@ -0,0 +1,14 @@
+import unittest
+import main_menu
+
+class TestMainMenu(unittest.TestCase):
+    def test_add_element(self):
+        MainMenu = []
+
+
+
+        self.assertEqual()
+
+
+
+unittest.main()
