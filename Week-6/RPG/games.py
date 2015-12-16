4925cae178deaa8b08eb7662e1ec2080708f4a25
Daniella Korossy <koda5055@gmail.com>
2015-12-16 11:49:10 +0100

    new starting point for the rpgame

---
+++ Week-6/RPG/games.py
@@ -0,0 +1,20 @@
+import main_menu
+import basic_elements
+import name_submenu
+import reroll_submenu
+import quit_submenu
+import potion_submenu
+
+class Game:
+    def start_game(self):
+        import main_menu
+        return
+
+
+
+
+game1 = Game()
+
+game1.start_game()
+
+basic_elements.player1.display_print()
