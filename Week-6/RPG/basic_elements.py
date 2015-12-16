8d315949af41f5a5f86beea3c27974eb2028b136
Daniella Korossy <koda5055@gmail.com>
2015-12-16 09:38:44 +0100

    refactors the choose method in class Menu

--- Week-6/RPG/basic_elements.py
+++ Week-6/RPG/basic_elements.py
@@ -4,37 +4,55 @@ class Menu():
 
     def printing_menu(self):
         for item in self.items:
-            print(str(item[0]) + ". " + str(item[1]))
+            print("\n" + str(item.number) + ". " + str(item.name) + "\n" + "----"*10)
 
     def add(self, item):
         self.items.append(item)
 
-    def choose(self):
+
+    def choice_input(self):
         while True:
             try:
                 choice_number = int(input("Enter the number of your choice: "))
-            except ValueError:
-                print("incorrect input")
-            else:
                 if choice_number > len(self.items):
                     print('out of range')
                 else:
-                    for item in self.items:
-                        if choice_number == item[0]:
-                            return item[2]()
-                    break
+                    return choice_number
+            except ValueError:
+                print("incorrect input")
+
+    def select_item(self, choice_number):
+        for item in self.items:
+            if choice_number == item.number:
+                return item
+
+    def choose_item(self):
+        choice_number = self.choice_input()
+        return self.select_item(choice_number)
+
+    def choose(self):
+        self.choose_item().cmd()
+
+    def choose_print(self):
+        print(self.choose_item().name)
+
 
 class MenuItem():
     def __init__(self, number, name, cmd):
         self.name = name
         self.number = number
         self.cmd = cmd
-        self.item = [self.number, self.name, self.cmd]
 
 
 class Player:
-    def __init__(self, name, dexterity, health, luck):
+    def __init__(self, name = "Anonim", dexterity = 0, health = 0, luck = 0):
         self.name = name
-        self.dext = dexterity
+        self.dexterity = dexterity
         self.health = health
         self.luck = luck
+
+    def display(self):
+        return {"name": self.name, "health":self.health, "dexterity": self.dexterity, "luck":self.luck}
+
+
+player1 = Player()        
