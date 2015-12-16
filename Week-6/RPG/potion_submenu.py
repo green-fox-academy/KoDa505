3eb21c925ad05a34978b8c704fdfe1fe2da7f67e
Daniella Korossy <koda5055@gmail.com>
2015-12-16 09:35:27 +0100

    creates potion submenu

---
+++ Week-6/RPG/potion_submenu.py
@@ -0,0 +1,14 @@
+from basic_elements import *
+
+
+Potion_SubMenu = Menu()
+
+
+Menu1 = MenuItem(1, "Potion of Dexterity", None)
+Menu2 = MenuItem(2, "Potion of Health", None)
+Menu3 = MenuItem(3, "Potion of Luck", None)
+
+
+Potion_SubMenu.add(Menu1)
+Potion_SubMenu.add(Menu2)
+Potion_SubMenu.add(Menu3)
