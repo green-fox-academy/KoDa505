00cb9eaca443e92f5c5c113af6c7ee48c6ddc7aa
Daniella Korossy <koda5055@gmail.com>
2015-12-16 13:32:05 +0100

    Player class in a new file

--- Week-6/RPG/basic_elements.py
+++ Week-6/RPG/basic_elements.py
@@ -1,3 +1,4 @@
+
 class Menu():
     def __init__(self):
         self.items = []
@@ -37,26 +38,8 @@ class Menu():
         print(self.choose_item().name)
 
 
-
 class MenuItem():
     def __init__(self, number, name, cmd):
         self.name = name
         self.number = number
         self.cmd = cmd
-
-
-class Player:
-    def __init__(self, name = "Anonim", dexterity = 0, health = 0, luck = 0):
-        self.name = name
-        self.dexterity = dexterity
-        self.health = health
-        self.luck = luck
-
-    def display(self):
-        return {"name": self.name, "health":self.health, "dexterity": self.dexterity, "luck":self.luck}
-
-    def display_print(self):
-        print("{} D: {} H: {} L: {}".format(self.name, self.dexterity, self.health, self.luck))
-
-
-player1 = Player()
