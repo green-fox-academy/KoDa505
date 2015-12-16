4575d4ba2b95ee3df242e43437e3aa9c7d3c2cbf
Daniella Korossy <koda5055@gmail.com>
2015-12-16 09:31:48 +0100

    changes the addition of one menu item

--- Week-6/RPG/reroll_submenu.py
+++ Week-6/RPG/reroll_submenu.py
@@ -1,13 +1,14 @@
 from basic_elements import *
 import commands_reroll_submenu
 
+
 Reroll_SubMenu = Menu()
 
 
-Menu1 = MenuItem(1, "Reroll stats", commands_reroll_submenu.reroll).item
-Menu2 = MenuItem(2, "Continue", "kuigig").item
-Menu3 = MenuItem(3, "Save", "save function").item
-Menu4 = MenuItem(4, "Quit", "save function").item
+Menu1 = MenuItem(1, "Reroll stats", commands_reroll_submenu.reroll)
+Menu2 = MenuItem(2, "Continue", commands_reroll_submenu.continuing)
+Menu3 = MenuItem(3, "Save", "save function")
+Menu4 = MenuItem(4, "Quit", commands_reroll_submenu.quitting)
 
 
 Reroll_SubMenu.add(Menu1)
