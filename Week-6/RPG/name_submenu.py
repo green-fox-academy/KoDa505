e87648785d5ef6df5d49c554a02cef2fdb7cd972
Daniella Korossy <koda5055@gmail.com>
2015-12-16 09:32:12 +0100

    changes the addition of one menu item

--- Week-6/RPG/name_submenu.py
+++ Week-6/RPG/name_submenu.py
@@ -4,10 +4,10 @@ import commands_name_submenu
 Name_SubMenu = Menu()
 
 
-Menu1 = MenuItem(1, "Reenter name", commands_name_submenu.reenter_name).item
-Menu2 = MenuItem(2, "Continue", commands_name_submenu.continuing).item
-Menu3 = MenuItem(3, "Save", "save function").item
-Menu4 = MenuItem(4, "Quit", commands_name_submenu.quitting).item
+Menu1 = MenuItem(1, "Reenter name", commands_name_submenu.reenter_name)
+Menu2 = MenuItem(2, "Continue", commands_name_submenu.continuing)
+Menu3 = MenuItem(3, "Save", commands_name_submenu.save)
+Menu4 = MenuItem(4, "Quit", commands_name_submenu.quitting)
 
 
 Name_SubMenu.add(Menu1)
--- Week-6/RPG/quit_submenu.py
+++ Week-6/RPG/quit_submenu.py
@@ -3,10 +3,12 @@ import commands_name_submenu
 
 Quit_SubMenu = Menu()
 
+def exiting():
+    exit()
 
-Menu1 = MenuItem(1, "Save and Quit", "com").item
-Menu2 = MenuItem(2, "Quit without save", "kuigig").item
-Menu3 = MenuItem(3, "Resume", "save function").item
+Menu1 = MenuItem(1, "Save and Quit", "sfhoi")
+Menu2 = MenuItem(2, "Quit without save", exiting)
+Menu3 = MenuItem(3, "Resume", "save function")
 
 
 Quit_SubMenu.add(Menu1)
