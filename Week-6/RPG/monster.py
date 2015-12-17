f1c56ddcf19b205a49ee39ccf003a150b73445e5
Daniella Korossy <koda5055@gmail.com>
2015-12-17 09:33:43 +0100

    create a monster class

---
+++ Week-6/RPG/monster.py
@@ -0,0 +1,29 @@
+import random
+
+class Monster:
+    def __init__(self, name, dexterity, health):
+        self.name = name
+        self.dexterity = dexterity
+        self.health = health
+
+    def display(self):
+        return {
+            "name": self.name, "health":self.health, "dexterity": self.dexterity
+        }
+
+    def display_print(self):
+        print("\n{} \nD: {} H: {} L: {} \nInventory: \n   Sword: {}\n   Leather armor: {}\n   Potion: {} ".format(self.name, self.dexterity, self.health, self.luck, self.sword, self.leather_armor, self.potion))
+
+    def shuffle_properties(self):
+        d = random.randint(1,15)
+        self.dexterity = d
+        d1 = random.randint(1,30)
+        self.health = d1
+
+    def fight_print(self):
+        print("\nYour enemy's name: {} \n   Its dexterity: {}\n   Its  health: {}".format(self.name, self.dexterity, self.health))
+
+
+
+monster = Monster("John", 1, 1)
+monster.shuffle_properties()
